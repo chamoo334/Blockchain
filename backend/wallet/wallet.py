55d2b01fcd564970e13b074d11a295b06e0f5e66
cm <cm@pop-os.localdomain>
2022-09-27 12:54:15 -0700

    blockchain basics completed

---
+++ backend/wallet/wallet.py
@@ -0,0 +1,7 @@
+class Wallet:
+    """
+    Individual wallet for a miner. Keeps track of miner's balance and allows miner to authorize transactions.
+    """
+
+    def __init__(self):
+        pass
