1051252e7c8dd0539ce15de582d9f4bcb2b28d6b
chamoo334 <cm@pop>
2022-10-06 16:23:16 -0700

    updating shutdown functionality

--- backend/port_selector/__init__.py
+++ backend/port_selector/__init__.py
@@ -1,8 +1,10 @@
 import random
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from backend.config import APP_PORT, PEER_HELPER_PORT
+from backend.scripts.server_ending_functions import initial_check, copy_ports_in_use
 
-PORTS_IN_USE = [APP_PORT, PEER_HELPER_PORT]
+PORT = PEER_HELPER_PORT
+PORTS_IN_USE = [APP_PORT, PORT]
 
 app = Flask(__name__)
 
@@ -12,8 +14,8 @@ def route_default():
 
 @app.route('/peer/port')
 def route_peer_port():
-    RANGE_START = PEER_HELPER_PORT + 1
-    RANGE_END = PEER_HELPER_PORT + 1000
+    RANGE_START = PORT + 1
+    RANGE_END = PORT + 1000
     PEER_PORT = random.randint(RANGE_START, RANGE_END)
 
     while PEER_PORT in PORTS_IN_USE:
@@ -23,5 +25,21 @@ def route_peer_port():
 
     return jsonify({ 'peer_port': PEER_PORT})
 
-print(PORTS_IN_USE)
-app.run(port=PEER_HELPER_PORT, host='0.0.0.0')
+@app.route('/remove/port', methods=['POST'])
+def route_remove_port():
+    remove_port = request.get_json()
+    print(remove_port)
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown_port_selector():
+    print('Shutdown context hit with POST!')
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if shutdown is None:
+        raise RuntimeError('The function is unavailable!')
+    else:
+        initial_check()
+        shutdown()
+
+if __name__ == '__main__':
+    print(PORTS_IN_USE)
+    app.run(port=PORT, host='0.0.0.0')
