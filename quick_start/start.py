7623c55f14b45bf48f30812a1fa92a7c51d2b2ce
chamoo334 <cm@pop>
2022-10-14 10:41:56 -0700

    quick_start.start initialized

--- quick_start/start.py
+++ quick_start/start.py
@@ -1,21 +1,23 @@
 from quick_start.parser import Parser
+from backend.util.update_peer_port import project_base_dir
 
 script_description = 'Blockchain initialization script. Requires argument for number of peers.'
 commands = {'peers': ['p', 'Specify number of peer instances to create. Will default to 1 if no input given', int]}
 starter = Parser(script_description, commands)
-total_peers = 1 if starter.args.peers is None else starter.args.peers
+starter.args.peers = 1 if starter.args.peers is None else starter.args.peers
+base_dir = project_base_dir()
+dockerfile_dir = f'{base_dir}/dockerfiles'
 
-
-#  start port_selector
+# start port_selector
 
 # start main api backend and frontend
 
 # start up peer instances
 
 
-def test():
-    print(total_peers)
+def main():
+    print('peer: ', starter.args.peers)
 
 
 if __name__ == '__main__':
-    test()
+    main()
